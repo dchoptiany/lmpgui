1968665e94776c30002c0394ef097de59a8f7d8c
Damian Choptiany <damian.choptiany@gmail.com>
2021-10-22 17:34:29 +0200

    Changed minRPMRel type to float

--- LmpGUI.py
+++ LmpGUI.py
@@ -99,7 +99,7 @@ def loadSettings():
 	scale = config.getfloat("LMPGUI", "scale")
 	indicatorsON = config.getboolean("LMPGUI", "indicatorsON")
 	speedInMPH = config.getboolean("LMPGUI", "speedInMPH")
-	minRPMRel = config.getint("LMPGUI", "minRPMRel")
+	minRPMRel = config.getfloat("LMPGUI", "minRPMRel")
 
 def saveSettings():
 	global config, scale, indicatorsON
@@ -161,9 +161,8 @@ def getSpeed():
 def drawRPMLights():
 	global scale, maxRPM, minRPMRel
 
-	minRPMNorm = minRPMRel / 100.0
 	currentRPM = ac.getCarState(0, acsys.CS.RPM)
-	numberOfLights = round(12 * ((currentRPM / maxRPM) - minRPMNorm) / (1 - minRPMNorm))
+	numberOfLights = round(12 * ((currentRPM / maxRPM) - minRPMRel) / (1 - minRPMRel))
 
 	numberOfLights = min(numberOfLights, 12)
 
